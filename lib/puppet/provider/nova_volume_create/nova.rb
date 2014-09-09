1d208882a2fe473cb3d9d53e8a0f555c13826e38
Atze de Vries <atze.devries@naturalis.nl>
2014-09-09 13:54:11 +0200

    some code cleanup

--- lib/puppet/provider/nova_volume_create/nova.rb
+++ lib/puppet/provider/nova_volume_create/nova.rb
@@ -5,15 +5,31 @@ Puppet::Type.type(:nova_volume_create).provide(:nova) do
   commands nova: 'nova'
 
   def exists?
-    nova('volume-list', '--os-auth-url', 'http://10.41.1.1:5000/v2.0', '--os-tenant-name', 'fileservers', '--os-tenant-id', 'cd707e8e349745bda47416179e4f537f', '--os-username', 'admin', '--os-password', resource[:password]).match(/^#{resource[:name]}$/)
+    nova('--os-auth-url', 'http://10.41.1.1:5000/v2.0',
+         '--os-tenant-name', 'fileservers',
+         '--os-tenant-id', 'cd707e8e349745bda47416179e4f537f',
+         '--os-username', 'admin',
+         '--os-password', resource[:password],
+         'volume-list').match(/^#{resource[:name]}$/)
   end
 
   def create
-    nova('volume-create', '1', '--display-name', resource[:name], '--os-auth-url', 'http://10.41.1.1:5000/v2.0', '--os-tenant-name', 'fileservers', '--os-tenant-id', 'cd707e8e349745bda47416179e4f537f', '--os-username', 'admin', '--os-password', resource[:password])
+    nova('--os-auth-url', 'http://10.41.1.1:5000/v2.0',
+         '--os-tenant-name', 'fileservers',
+         '--os-tenant-id', 'cd707e8e349745bda47416179e4f537f',
+         '--os-username', 'admin',
+         '--os-password', resource[:password],
+         'volume-create', '1',
+         '--display-name', resource[:name])
   end
 
   def destory
-    nova('volume-delete', resource[:name], '--os-auth-url', 'http://10.41.1.1:5000/v2.0', '--os-tenant-name', 'fileservers', '--os-tenant-id', 'cd707e8e349745bda47416179e4f537f', '--os-username', 'admin', '--os-password', resource[:password])
+    nova('--os-auth-url', 'http://10.41.1.1:5000/v2.0',
+         '--os-tenant-name', 'fileservers',
+         '--os-tenant-id', 'cd707e8e349745bda47416179e4f537f',
+         '--os-username', 'admin',
+         '--os-password', resource[:password],
+         'volume-delete', resource[:name])
   end
 
 end
