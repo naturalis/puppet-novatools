7acdfc9da16af0b06558549e8f00365c7aef597e
Atze de Vries <atze.devries@naturalis.nl>
2014-09-10 11:30:48 +0200

    added attaching function

--- lib/puppet/provider/nova_volume_attach/nova.rb
+++ lib/puppet/provider/nova_volume_attach/nova.rb
@@ -5,12 +5,25 @@ Puppet::Type.type(:nova_volume_attach).provide(:nova) do
   commands nova: 'nova'
 
   def exists?
+    vi = get_volume_info
+    id = get_instance_id
+    if id == "nod found"
+      raise ArgumentError, "Instance %s is not found" % resource[:instance]
+    end
+    if vi["attached_to"] == id
+      true
+    elsif vi["attached_to"] == ""
+      false
+    else
+      raise ArgumentError, "Volume %s is attached to different instance" % resource[:name]
+    end
+
     # nova('--os-auth-url', "http://#{resource[:controller_ip]}:5000/v2.0",
     #      '--os-tenant-name', resource[:tenant],
     #      '--os-username', resource[:username],
     #      '--os-password', resource[:password],
     #      'volume-list').match("#{resource[:name]}")
-    false
+
   end
 
   def create
@@ -48,6 +61,23 @@ Puppet::Type.type(:nova_volume_attach).provide(:nova) do
       end
     end
     return volume_info
+  end
+
+  def get_instance_id
+    instance_id = "not found"
+    vid = nova('--os-auth-url', "http://#{resource[:controller_ip]}:5000/v2.0",
+               '--os-tenant-name', resource[:tenant],
+               '--os-username', resource[:username],
+               '--os-password', resource[:password],
+               'list')
+    vid = vid.split("\n")
+    vid.each do |v|
+      if v.include? resource[:instance]
+        r = v.split('|')
+        instance_id = r[1]
+      end
+    end
+    return instance_id
   end
 
 end
