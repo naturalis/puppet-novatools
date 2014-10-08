e15b6ae65d2504ad51299b9fa40849380a9c4369
Atze de Vries <atze.devries@naturalis.nl>
2014-10-08 09:26:35 +0200

    set create volume to api version

--- lib/puppet/provider/nova_volume_attach/nova.rb
+++ lib/puppet/provider/nova_volume_attach/nova.rb
@@ -4,37 +4,47 @@ Puppet::Type.type(:nova_volume_attach).provide(:nova) do
 
   commands nova: 'nova'
 
+  @token = false
+
   def exists?
-    vi = get_volume_info
-    id = get_instance_id
-    if id == "not found"
-      raise "Instance %s is not found" % resource[:instance]
-    end
-    if vi["attached_to"] == id
-      true
-    elsif vi["attached_to"] == ""
-      false
-    else
-      raise "Volume %s is attached to different instance" % resource[:name]
-    end
+    # JOBS
+    # Check if volume exists
+    # Check if voume is attached to something
+
+    # vi = get_volume_info
+    # id = get_instance_id
+    # if id == "not found"
+    #   raise "Instance %s is not found" % resource[:instance]
+    # end
+    # if vi["attached_to"] == id
+    #   true
+    # elsif vi["attached_to"] == ""
+    #   false
+    # else
+    #   raise "Volume %s is attached to different instance" % resource[:name]
+    # end
+    find_volume
+    attach_info
   end
 
   def create
-    vi = get_volume_info
-    nova('--os-auth-url', "http://#{resource[:controller_ip]}:5000/v2.0",
-         '--os-tenant-name', resource[:tenant],
-         '--os-username', resource[:username],
-         '--os-password', resource[:password],
-         'volume-attach', resource[:instance], vi['id'])
+    # vi = get_volume_info
+    # nova('--os-auth-url', "http://#{resource[:controller_ip]}:5000/v2.0",
+    #      '--os-tenant-name', resource[:tenant],
+    #      '--os-username', resource[:username],
+    #      '--os-password', resource[:password],
+    #      'volume-attach', resource[:instance], vi['id'])
+    puts 'create to be implemented'
   end
 
   def destroy
-    vi = get_volume_info
-    nova('--os-auth-url', "http://#{resource[:controller_ip]}:5000/v2.0",
-         '--os-tenant-name', resource[:tenant],
-         '--os-username', resource[:username],
-         '--os-password', resource[:password],
-         'volume-detach', resource[:instance], vi['id'])
+    # vi = get_volume_info
+    # nova('--os-auth-url', "http://#{resource[:controller_ip]}:5000/v2.0",
+    #      '--os-tenant-name', resource[:tenant],
+    #      '--os-username', resource[:username],
+    #      '--os-password', resource[:password],
+    #      'volume-detach', resource[:instance], vi['id'])
+    puts 'destroy to be implemented'
   end
 
   def get_volume_info
@@ -73,5 +83,115 @@ Puppet::Type.type(:nova_volume_attach).provide(:nova) do
     end
     return instance_id
   end
+
+  def volume_info
+    update_token
+    volume_endpoint = String.new
+    @token['access']['serviceCatalog'].each do |endpoint|
+      volume_endpoint = endpoint['endpoints'][0]['publicURL'] if endpoint['type'].include? 'volume'
+      # if endpoint['type'].include? 'volume'
+      #   volume_endpoint = endpoint['endpoints'][0]['publicURL']
+      # end
+    end
+
+    uri = URI("#{volume_endpoint}/volumes")
+    http = Net::HTTP.new(uri.host, uri.port)
+    req = Net::HTTP::Get.new(uri.path)
+    req['x-auth-token'] = @token['access']['token']['id']
+    req['content-type'] = 'application/json'
+    req['accept'] = 'application/json'
+    res = http.request(req)
+    JSON.parse(res.body)
+  end
+
+  def attach_info
+    puts volume_info
+  end
+
+  def find_volume
+    update_token
+    volume_endpoint = String.new
+    @token['access']['serviceCatalog'].each do |endpoint|
+      if endpoint['type'].include? 'volume'
+        volume_endpoint = endpoint['endpoints'][0]['publicURL']
+      end
+    end
+
+    uri = URI("#{volume_endpoint}/volumes")
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    req = Net::HTTP::Get.new(uri.path)
+    req['x-auth-token'] = @token['access']['token']['id']
+    req['content-type'] = 'application/json'
+    req['accept'] = 'application/json'
+    res = http.request(req)
+    res = JSON.parse(res.body)
+    if res['volumes'].empty?
+      return false
+    else
+      res['volumes'].each do |v|
+        if v['display_name'].include? resource[:name]
+          puts 'volume found'
+          return true
+        else
+          puts 'volume not found'
+          return false
+        end
+      end
+    end
+  end
+
+  def create_volume
+    update_token
+
+    volume_endpoint = String.new
+    @token['access']['serviceCatalog'].each do |endpoint|
+      if endpoint['type'].include? 'volume'
+        volume_endpoint = endpoint['endpoints'][0]['publicURL']
+      end
+    end
+
+    uri = URI("#{volume_endpoint}/volumes")
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    req = Net::HTTP::Post.new(uri.path)
+    volume_data = { 'volume' => {'size' => '1', 'display_name' => resource[:name] } }
+    req.body = volume_data.to_json
+    req['x-auth-token'] = @token['access']['token']['id']
+    req['content-type'] = 'application/json'
+    req['accept'] = 'application/json'
+    res = http.request(req)
+
+    JSON.parse(res.body)
+  end
+
+  def openstack_auth
+    uri = URI("http://#{resource[:controller_ip]}:5000/v2.0/tokens")
+    http = Net::HTTP.new(uri.host, uri.port)
+    req = Net::HTTP::Post.new(uri.path)
+    auth_data = { 'auth' => { 'tenantName' => resource[:tenant], 'passwordCredentials' => { 'username' => resource[:username], 'password' => resource[:password] } } }
+    req.body = auth_data.to_json
+    req['content-type'] = 'application/json'
+    req['accept'] = 'application/json'
+    res = http.request(req)
+
+    JSON.parse(res.body)
+  end
+
+  def update_token
+    if @token
+      puts 'token exists'
+      expire = Time.parse(@token['access']['token']['expires']) - 60
+      # if Time.now > expire then
+      #   token = openstack_auth
+      # end
+      puts 'token expired, requesting new' if Time.now > expire
+      @token = openstack_auth if Time.now > expire
+    else
+      puts 'token created'
+      @token = openstack_auth
+    end
+  end
+
 
 end
