6189ccfefe307cd765b1c25dcde9bc7cbbc03efe
Atze de Vries <atze.devries@naturalis.nl>
2015-04-22 15:22:09 +0200

    fix for recreating volumes

--- lib/puppet/provider/nova_volume/nova_volume.rb
+++ lib/puppet/provider/nova_volume/nova_volume.rb
@@ -10,7 +10,6 @@ Puppet::Type.type(:nova_volume).provide(:nova_volume) do
   def exists?
     ep = URI(resource[:keystone_endpoint])
     @property_hash[:nova] = OpenStackAPI.new(ep.host,ep.port,ep.path,resource[:username],resource[:password],resource[:tenant])
-    list_devices
     result = check_volume_exists
     result = is_volume_attached if resource[:attach_volume]
     result = is_volume_formatted unless resource[:create_filesystem] == 'false'
